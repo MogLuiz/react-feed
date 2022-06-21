8a0355380bb12b49a3a3d86c8ee11c112653960f
Luiz Henrique <luizhjramos@outlook.com>
2022-06-21 09:55:05 -0300

    feat(Header): header component implementation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+
+import igniteLogo from '../../assets/ignite-logo.svg'
+
 import styles from "./styles.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logo do projeto" />
     </header>
   );
 };
