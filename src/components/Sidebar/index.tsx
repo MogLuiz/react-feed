509552f948e5ebab31e36bfd2688bc11c2cdf3fe
Luiz Henrique <luizhjramos@outlook.com>
2022-06-21 21:43:50 -0300

    styles(sidebar): sidebar styling

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <img src="https://images.unsplash.com/photo-1605379399843-5870eea9b74e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"  />
-    
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399843-5870eea9b74e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/58401291?v=4" />
+
         <strong>Luiz Ramos</strong>
         <span>Frontend Engineer</span>
       </div>
 
       <footer>
-        <a href="#">
-          Editar o seu perfil
-        </a>
+        <a href="#">Editar o seu perfil</a>
       </footer>
     </aside>
-  )
+  );
 };
 
 export default Sidebar;
