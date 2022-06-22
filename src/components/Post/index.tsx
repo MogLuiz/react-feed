8b354a408c062b23aba248ecee30a97d8426a8f7
Luiz Henrique <luizhjramos@outlook.com>
2022-06-22 14:05:14 -0300

    feat(post): post header semantic html structure

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import styles from "./styles.module.css";
+
 type TPostProps = {
   author: string;
   content: string;
@@ -7,11 +9,20 @@ type TPostProps = {
 
 const Post = ({ author, content }: TPostProps) => {
   return (
-    <div>
-        <strong>{author}</strong>
-        <p>{content}</p>
-    </div>
-  )
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/58401291?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Luiz Ramos</strong>
+            <span>Frontend Engineer</span>
+          </div>
+        </div>
+
+        <time dateTime="2022-05-22">Publicado há 1h</time>
+      </header>
+    </article>
+  );
 };
 
 export default Post;
