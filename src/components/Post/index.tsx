f1130d4f5746d6a4fa83157126495f72fba9196f
Luiz Henrique <luizhjramos@outlook.com>
2022-06-26 19:26:48 -0300

    feat(post): dynamic post content

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -52,18 +52,15 @@ const Post = ({
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
-          <a href="#"> #rocketseat</a>
-        </p>
+        {content.map(({ content, type }: TContentPost) => {
+          if (type === "paragraph") return <p>{content}</p>;
+          if (type === "link")
+            return (
+              <p>
+                <a href="#">{content}</a>
+              </p>
+            );
+        })}
       </div>
 
       <form className={styles.commentForm}>
